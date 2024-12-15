522aa4ed90e196ab0f148c18a6cd9d9564f984ef
visheshdubey <visheshdubey2016@gmail.com>
2024-12-15 11:26:23 +0530

    feat: add Radix UI components and enhance AppHeader for user session handling
    - Added @radix-ui/react-avatar and @radix-ui/react-dropdown-menu dependencies to package.json and package-lock.json.
    - Updated AppHeader component to include AppLogo and LoggedInUserProfileMenu.
    - Enhanced AppHeader to conditionally render sign-in link based on user session status.

---
+++ frontend/src/features/navigation/components/LoggedInUserProfileMenu.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/lib/shadcn/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/lib/shadcn/components/ui/dropdown-menu";
+import { signOut, useSession } from "next-auth/react";
+
+import Link from "next/link";
+import { get } from "lodash";
+import { useRouter } from "next/navigation";
+
+type Props = {};
+
+const ProfileMenu = (props: Props) => {
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar className="size-7">
+                    <AvatarImage src={get(session, "user.image")} />
+                    <AvatarFallback>{get(session, "user.name", "Polka Fi")?.charAt(0)}</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {/* <DropdownMenuItem onClick={() => router.push("/settings")}>My Account</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => router.push("/settings")}>Settings</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link href="mailto:visheshdubey.work@gmail.com">Request credits</Link>
+                </DropdownMenuItem>*/}
+                <DropdownMenuItem>
+                    <Link href="mailto:visheshdubey.work@gmail.com">Feedbacks</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOut()}>Log Out</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export default ProfileMenu;
